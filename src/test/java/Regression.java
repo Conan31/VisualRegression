38596603c43f1618acd0435046e69692b7ca6278
Alexander Marichev <conan31@mail.ru>
2018-11-29 12:22:58 +0300

    Added reporting - part_1

--- src/test/java/Regression.java
+++ src/test/java/Regression.java
@@ -1,23 +1,28 @@
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.util.concurrent.TimeUnit;
 
 public class Regression {
     WebDriver driver;
+    ExtentReports report;
+    ExtentTest test;
 
     @BeforeClass
     public void beforeClass()
     {
+        report = new ExtentReports(System.getProperty("user.dir") + "/report/Report.html");
+
         try {
             FileUtils.cleanDirectory(new File(System.getProperty("user.dir") + "/src/images/screenshots"));
             FileUtils.cleanDirectory(new File(System.getProperty("user.dir") + "/src/images/diffimages"));
@@ -38,10 +43,16 @@ public class Regression {
         driver.findElement(By.name("login")).click();
     }
 
+    @BeforeMethod
+    public void beforeMethod(Method method)
+    {
+        test = report.startTest(method.getName());
+    }
+
     @DataProvider(name="urls")
     public static Object[][] urls(){
 
-        return new Object[][] {
+        return new Object[][]{
 
                 {"http://newtours.demoaut.com/mercurywelcome.php", "homePage"},
                 {"http://newtours.demoaut.com/mercuryreservation.php", "reservationPage"},
@@ -64,5 +75,8 @@ public class Regression {
     public void afterClass()
     {
       driver.quit();
+
+      report.endTest(test);
+      report.flush();
     }
 }
